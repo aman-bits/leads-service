26d344c09ff2b4b95c7167e8d55c46cce88c06c1
Aman Kumar <aman.kumar@builder.ai>
2024-11-24 10:12:10 +0530

    docker compose file added - database updated to postgres

--- leadservice/settings.py
+++ leadservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'leadservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'lead_management_db'),
+        'USER': os.getenv('POSTGRES_USER', 'lead_admin'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'strongpassword'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # Use the service name of the database container
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
